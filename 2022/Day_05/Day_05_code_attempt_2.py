818d2408d7ab8cf2627593944fb2f3284da19317
Anna Garstka <62151461+anna-lcg@users.noreply.github.com>
2022-12-06 14:34:15 +0000

    used deepcopy and lists unlinked

--- 2022/Day_05/Day_05_code_attempt_2.py
+++ 2022/Day_05/Day_05_code_attempt_2.py
@@ -1,3 +1,5 @@
+import copy
+
 f = open("Advent of Code Python/2022/Day_05_input.txt", "r")
 puzzle_input = f.readlines()
 
@@ -5,8 +7,9 @@ break_line=puzzle_input.index('\n')
 original_stack=puzzle_input[0:break_line-1]
 instructions_draft=puzzle_input[break_line+1:]
 
-part_1_stack=[]
+stack=[]
 stack_draft=[[],[],[],[],[],[],[],[],[]]
+
 for line in original_stack:
     y=0
     for i in range(1,35,4):
@@ -17,9 +20,10 @@ for line_draft in stack_draft:
     line=line_draft
     while line[0]==' ':
         del line[0]
-    part_1_stack.append(line)
-# part_2_stack=list(part_1_stack)
+    stack.append(line)
 
+part_1_stack=copy.deepcopy(stack)
+part_2_stack=copy.deepcopy(stack)
 
 def format_instructions(ins):
     formated_ins=[]
@@ -38,7 +42,7 @@ def implement_part_1_instructions(ins,s):
     stack_output=s
 
     crate_load=stack_output[move_from][0:number_of_crates]
-    # crate_load.reverse()
+
     for i in crate_load:
         stack_output[move_to].insert(0,i)
     stack_output[move_from]=stack_output[move_from][number_of_crates:]
@@ -54,58 +58,29 @@ def implement_part_2_instructions(ins,s):
 
     crate_load=stack_output[move_from][0:number_of_crates]
     crate_load.reverse()
+
     for i in crate_load:
         stack_output[move_to].insert(0,i)
     stack_output[move_from]=stack_output[move_from][number_of_crates:]
 
     return stack_output
 
-
 for i in instructions:
     part_1_stack=implement_part_1_instructions(i,part_1_stack)
-
-
-#----------------------------------
-f = open("Advent of Code Python/2022/Day_05_input.txt", "r")
-puzzle_input = f.readlines()
-
-break_line=puzzle_input.index('\n')
-original_stack=puzzle_input[0:break_line-1]
-instructions_draft=puzzle_input[break_line+1:]
-
-stack_draft=[[],[],[],[],[],[],[],[],[]]
-part_2_stack=[]
-for line in original_stack:
-    y=0
-    for i in range(1,35,4):
-        stack_draft[y].append(line[i])
-        y+=1
-
-for line_draft in stack_draft:
-    line=line_draft
-    while line[0]==' ':
-        del line[0]
-    part_2_stack.append(line)
-#--------------------------
-for i in instructions:
     part_2_stack=implement_part_2_instructions(i,part_2_stack)
-
-
 
 def format_for_answer(stack):
     answer=[]
-    count=0
-    # print(stack)
+
     for i in stack:
-        # print(count, i)
         if len(i) >=1:
             x=i[0]
             answer.append(x)
         else:
             answer.append('')
-        count+=1
-    # print('answer:\n', answer)
+
     answer=''.join(answer)
+    
     return answer
 
 part_1_answer=format_for_answer(part_1_stack)
