fe03130b4c2b96dfff919b0b3b8fe293d38f872c
Anna Garstka <62151461+anna-lcg@users.noreply.github.com>
2023-12-01 22:31:11 +0000

    Update 2023 D01P1.py

--- 2023/Day_01/2023 D01P1.py
+++ 2023/Day_01/2023 D01P1.py
@@ -13,30 +13,6 @@ calibration = f.readlines()
 #for i in calibration:
    # print ("this line is:", i)
 individuals =[]
-digitlist=[1,2,3,4,5,6,7,8,9,0,'one','two','three','four','five','six','seven','eight','nine','zero']
-digitdic={
-    '1':1,
-    'one':1,
-    '2':2,
-    'two':2,
-    '3':3,
-    'three':3,
-    '4':4,
-    'four':4,
-    '5':5,
-    'five':5,
-    '6':6,
-    'six':6,
-    '7':7,
-    'seven':7,
-    '8':8,
-    'eight':8,
-    '9':9,
-    'nine':9,
-    '0':0,
-    'zero':0,
-}
-#print(digitdic['zero'])
 for i in calibration:
     this_line=[]
     for x in i:
