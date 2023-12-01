e035372f20a94d345e33445beb366d571f5d31c2
Anna Garstka <62151461+anna-lcg@users.noreply.github.com>
2023-12-01 22:36:05 +0000

    Add files via upload

---
+++ 2023/2023 D02C .py
@@ -0,0 +1,17 @@
+f = open('2023/2023 D02I.txt', "r")
+puzzle_input = f.readlines()
+
+#puzzle_input = ("""1abc2
+#pqr3stu8vwx
+#a1b2c3d4e5f
+#treb7uchet""")
+
+#print (puzzle_input)
+#puzzle_input =puzzle_input.split("\n")
+
+def Part_1(calibration):
+
+def Part_2(calibration):
+
+print ('The part 1 answer is', Part_1(puzzle_input))
+print ('The part 2 answer is', Part_2(puzzle_input))
