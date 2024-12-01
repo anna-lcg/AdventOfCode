44713b26839abd658ab438606a37bfc0c4162c4d
Anna Garstka <62151461+anna-lcg@users.noreply.github.com>
2024-12-01 18:39:20 +0000

    Update Day_25.py

--- 2023/Day_25/Day_25.py
+++ 2023/Day_25/Day_25.py
@@ -1,1 +1,16 @@
+f = open('2023/2023 D25I.txt', "r")
+puzzle_input = f.readlines()
 
+#puzzle_input = ("""1abc2
+#pqr3stu8vwx
+#a1b2c3d4e5f
+#treb7uchet""")
+#puzzle_input =puzzle_input.split("\n")
+#print (puzzle_input)
+
+def Part_1(calibration):
+
+def Part_2(calibration):
+
+print ('The part 1 answer is', Part_1(puzzle_input))
+print ('The part 2 answer is', Part_2(puzzle_input))
