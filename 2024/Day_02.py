7ea65cd852fa088a9e09ff4aadd8c7bf13a7f216
Anna Garstka <62151461+anna-lcg@users.noreply.github.com>
2024-12-01 18:23:12 +0000

    Add files via upload

---
+++ 2024/Day_02.py
@@ -0,0 +1,27 @@
+textinputstr = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+textinputstr=textinputstr.split("\n")
+
+f = open('Day02I.txt', "r")
+puzzle_input = f.readlines()
+
+textinput=[]
+for pair in textinputstr:
+    textinput.append(pair.split("   "))
+
+#def Process1(source):
+
+#def Part1(SourceA,SourceB):
+ 
+#def Part2(SourceA,SourceB):
+
+
+
+#processedinput=Process1(textinput)
+#print(processedinput)
+#print(Part1(processedinput[0],processedinput[1]))
+#print(Part2(processedinput[0],processedinput[1]))
